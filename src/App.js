7cf5a4ad5bcf0f601bf47043bc666e61f2e7823e
rafikamirkarimova <tekhronkhudaykulov@gmail.com>
2021-10-22 20:10:50 +0500

    dasda

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import '../src/App.scss'
 import {
     BrowserRouter,
@@ -6,22 +6,53 @@ import {
     Route,
 } from "react-router-dom";
 import 'antd/dist/antd.css';
-import { Provider } from "react-redux";
 import { LoginPage, RegisterPage } from "./components/pages";
-import store from "./store/store";
+import {Link,useHistory} from 'react-router-dom';
+import { setProfile } from "./store/actions/profile";
+import { connect } from "react-redux";
+import axios from 'axios';
+import ReactLoading from 'react-loading';
 
 
-export default function App() {
+
+function App() {
+    const history = useHistory();
+    const [isLoading, setIsLoading] = useState(false);
+
+    const getUser = async () => {
+        try{
+            const token =  localStorage.getItem('token');
+            if(token){
+                
+            }
+            setIsLoading(true)
+              const res = await axios.get('https://api-nodejs-todolist.herokuapp.com/user/user/me');
+              localStorage.setItem('token', res.data.token);
+              setProfile(res.data);
+              history.push('/home');
+          }catch (e) {
+      
+          }finally{
+            setIsLoading(false);
+          }
+    }
+    useEffect(() => {
+        getUser();
+    }, [])
     return (
         <>
-        <Provider store={store}>
         <BrowserRouter>
                 <Switch>
                     <Route path="/" component={LoginPage} exact/>
                     <Route path="/register" component={RegisterPage} exact/>
                 </Switch>
             </BrowserRouter>
-        </Provider>
         </>
     );
 }
+
+const mapDispatchToProps = {
+    setProfile
+}
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,12 +1,28 @@
+import React, {useState} from 'react';
 import { Form, Input, Button, Checkbox } from 'antd';
 import {Link,useHistory} from 'react-router-dom';
+import axios from 'axios';
+import ReactLoading from 'react-loading';
+import { connect } from 'react-redux';
+import { setProfile } from '../../store/actions/profile';
 
-const LoginPage = () => {
+const LoginPage = ({setProfile}) => {
   const history = useHistory();
+  const [isLoading, setIsLoading] = useState(false);
 
-  const onFinish = (values) => {
-    history.push('/login');
-  };
+  const onFinish = async (values) => {
+    try{
+      setIsLoading(true)
+        const res = await axios.post('https://api-nodejs-todolist.herokuapp.com/user/login', values);
+        localStorage.setItem('token', res.data.token);
+        setProfile(res.data);
+        history.push('/home');
+    }catch (e) {
+
+    }finally{
+      setIsLoading(false);
+    }
+  }
 
   const onFinishFailed = (errorInfo) => {
     console.log('Failed:', errorInfo);
@@ -23,26 +39,23 @@ const LoginPage = () => {
       wrapperCol={{
         span: 16,
       }}
-      initialValues={{
-        remember: true,
-      }}
       onFinish={onFinish}
       onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
-      <Form.Item
-        label="Username"
-        name="username"
+         <Form.Item
+        label="Email"
+        name="email"
         rules={[
           {
-            required: false,
+            type:'email',
+            required: true,
             message: 'Please input your username!',
           },
         ]}
       >
         <Input />
       </Form.Item>
-
       <Form.Item
         label="Password"
         name="password"
@@ -55,18 +68,6 @@ const LoginPage = () => {
       >
         <Input.Password />
       </Form.Item>
-
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
       <Form.Item
         wrapperCol={{
           offset: 8,
@@ -74,7 +75,9 @@ const LoginPage = () => {
         }}
       >
         <Button type="primary" htmlType="submit">
-          Submit
+          {
+            isLoading ? <ReactLoading width={20} type="spinningBubbles" color={"red"} /> : "Submit"
+          }
         </Button>
       </Form.Item>
       <div>
@@ -88,4 +91,9 @@ const LoginPage = () => {
   );
 };
 
-export default LoginPage;
+const mapDispatchToProps = {
+  setProfile
+}
+
+
+export default connect(null, mapDispatchToProps)(LoginPage);
